3149cfde8a084d6c3d84cbd467e11c697156fd43
喻波 <1142493958@qq.com>
2018-05-31 17:12:28 +0800

    增加弹框

--- my-mall/src/components/commons/ProductList.vue
+++ my-mall/src/components/commons/ProductList.vue
@@ -34,7 +34,10 @@
                                 <span>{{ props.row.address }}</span>
                             </el-form-item>
                             <el-form-item label='商品描述'>
-                                <span>{{ props.row.desc }}</span>
+                                <img src="../../assets/logo.png" alt="" class="img">
+                                <img src="../../assets/logo.png" alt="" class="img">
+                                <img src="../../assets/logo.png" alt="" class="img">
+                                <img src="../../assets/logo.png" alt="" class="img">
                             </el-form-item>
                             </el-form>
                         </template>
@@ -48,8 +51,28 @@
                         prop='name'>
                         </el-table-column>
                         <el-table-column
-                        label='描述'
+                        label='分类'
                         prop='desc'>
+                        </el-table-column>
+                        <el-table-column
+                        fixed="right"
+                        label="操作"
+                        width="200">
+                        <template slot-scope="scope">
+                            <el-button
+                            @click.native.prevent="deleteRow(scope.$index, tableData)"
+                            type="text"
+                            size="small">
+                            上架
+                            </el-button>
+                            <el-button type="text" @click.native.prevent="deleteRow(scope.$index, tableData)">编辑</el-button>
+                            <el-button
+                            @click.native.prevent="deleteRow(scope.$index, tableData)"
+                            type="text"
+                            size="small">
+                            删除
+                            </el-button>
+                        </template>
                         </el-table-column>
                     </el-table>
                 </div>
@@ -73,6 +96,58 @@
                 </div>
             </el-col>
         </el-row>
+
+        <el-dialog title="商品编辑" :visible.sync="editForm" :before-close="handleClose" class="ed">
+            <el-form ref="form" :model="form" label-width="80px">
+                <el-form-item label="活动名称">
+                    <el-input v-model="form.name"></el-input>
+                </el-form-item>
+                <el-form-item label="活动区域">
+                    <el-select v-model="form.region" placeholder="请选择活动区域">
+                    <el-option label="区域一" value="shanghai"></el-option>
+                    <el-option label="区域二" value="beijing"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="活动时间">
+                    <el-col :span="11">
+                    <el-date-picker type="date" placeholder="选择日期" v-model="form.date1" style="width: 100%;"></el-date-picker>
+                    </el-col>
+                    <el-col class="line" :span="2">-</el-col>
+                    <el-col :span="11">
+                    <el-time-picker type="fixed-time" placeholder="选择时间" v-model="form.date2" style="width: 100%;"></el-time-picker>
+                    </el-col>
+                </el-form-item>
+                <el-form-item label="即时配送">
+                    <el-switch v-model="form.delivery"></el-switch>
+                </el-form-item>
+                <el-form-item label="活动性质">
+                    <el-checkbox-group v-model="form.type">
+                    <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
+                    <el-checkbox label="地推活动" name="type"></el-checkbox>
+                    <el-checkbox label="线下主题活动" name="type"></el-checkbox>
+                    <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
+                    </el-checkbox-group>
+                </el-form-item>
+                <el-form-item label="特殊资源">
+                    <el-radio-group v-model="form.resource">
+                    <el-radio label="线上品牌商赞助"></el-radio>
+                    <el-radio label="线下场地免费"></el-radio>
+                    </el-radio-group>
+                </el-form-item>
+                <el-form-item label="活动形式">
+                    <el-input type="textarea" v-model="form.desc"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="onSubmit">立即创建</el-button>
+                    <el-button>取消</el-button>
+                </el-form-item>
+            </el-form>
+            <!-- <div  class="dialog-footer">
+                <el-button @click="editFormVisible = false">取消</el-button>
+                <el-button type="primary" @click="editSubmit">提交</el-button>
+            </div> -->
+        </el-dialog>
+
     </div>
 </template>
 <script>
@@ -115,7 +190,18 @@ export default {
         shop: '王小虎夫妻店',
         shopId: '10333'
       }],
-      currentPage4: 4
+      currentPage4: 4,
+      editForm: false,
+      form: {
+        name: '',
+        region: '',
+        date1: '',
+        date2: '',
+        delivery: false,
+        type: [],
+        resource: '',
+        desc: ''
+      }
     }
   },
   mounted () {
@@ -134,6 +220,16 @@ export default {
     },
     handleCurrentChange (val) {
       console.log(`当前页: ${val}`)
+    },
+    editSubmit () {
+      console.log(1)
+    },
+    deleteRow (index, rows) {
+      this.editForm = true
+      console.log(this.editForm)
+    },
+    handleClose () {
+      this.editForm = false
     }
   }
 }
@@ -151,4 +247,8 @@ export default {
     margin-bottom: 0;
     width: 50%;
   }
+.img{
+    width: 100px;
+    border: 1px solid #ededed
+}
 </style>
