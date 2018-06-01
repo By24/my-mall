b80e0af5366a754d56f1e7ad8a6003aef34acf09
喻波 <1142493958@qq.com>
2018-06-01 18:34:27 +0800

    ..

--- my-api/routers/main.js
+++ my-api/routers/main.js
@@ -1,7 +1,15 @@
 var express = require('express')
 var Title = require('../models/Title')
 var router = express.Router();
-
+const qn = require('qn');
+var fs = require('fs');
+const bucket = 'avatar-img-d';
+const client = qn.create({
+    accessKey: 'n83SaVzVtzNbZvGCz0gWsWPgpERKp0oK4BtvXS-Y',
+    secretKey: '1Uve9T2_gQX9pDY0BFJCa1RM_isy9rNjfC4XVliW',
+    bucket: bucket,
+    origin: 'http://ouibvkb9c.bkt.clouddn.com'
+})
 //同一返回格式
 var responseData;
 router.use(function (req, res, next) {
@@ -41,6 +49,47 @@ router.get('/navigation', function (req, res, next) {
         responseData.message = '添加成功';
         res.json(responseData);
     })
+})
+// 上传
+router.all('/upload',  (req, res, next) => {
+    console.log(req.body)
+    res.json(req.body)
+    return
+    var imgData = req.body.imgData || "10321321";
+    // 构建名
+    var fileName = Date.now() + '.mp3';
+    // 构建路径
+    var filePath = './image/' + fileName;
+    // 过滤data:URL
+    // var base64Data = imgData.replace(/^data:image\/\w+;base64,/, "");
+    var dataBuffer = new Buffer(imgData, 'base64' );
+    try {
+        fs.writeFile(filePath, dataBuffer, function (err) {
+            if (err) {
+                res.end(JSON.stringify({status: '102', msg: '文件写入失败'}));
+            } else {
+                client.uploadFile(filePath, {key: `/avatar/${fileName}`}, function (err1, result) {
+
+                    res.json({
+                        status: '0',
+                        result: {
+                            path: result.url
+                        },
+                        msg: 'suc'
+                    })
+                    
+                    // 上传之后删除本地文件
+                    fs.unlinkSync(filePath);
+                });
+            }
+        })
+    } catch (err) {
+        res.json({
+            status: '1',
+            msg: err.message
+        });
+    }
+
 })
 
 module.exports = router;
--- my-mall/src/components/Home.vue
+++ my-mall/src/components/Home.vue
@@ -13,10 +13,49 @@
       <div class="card_titel"><p class="card_name">{{item.name}}</p><a href="" class="card_more">更多 ></a></div>
       <div class="card_cont">
         <div class="card_cont_left">
-
+          <img src="../assets/timg.jpg" alt="">
         </div>
-        <div class="card_cont_right">
 
+        <div class="card_cont_right">
+          <div class="good-item" >
+            <div><div class="good-img">
+              <a href="/goodsDetails?productId=100022201" class="">
+              <img alt="《深泽直人》" src="http://image.smartisanos.cn/resource/5e4e40120d09fb6791f9430f914c6f68.jpg" lazy="loaded">
+              </a>
+              </div> 
+              <h6 class="good-title">《深泽直人》</h6> 
+              <h3 class="sub-title ellipsis">首次面向中国读者介绍其作品</h3> 
+              <div class="good-price pr">
+                <div class="ds pa">
+                <a href="/goodsDetails?productId=100022201" class="">
+                  <input data-v-02725a06="" type="button" readonly="readonly" class="default-btn" value="查看详情" style="margin: 0px 5px;">
+                </a> 
+                <input data-v-02725a06="" type="button" readonly="readonly" class="main-btn" value="加入购物车" style="margin: 0px 5px;">
+                </div>
+                  <p><span style="font-size: 16px;">￥</span>199</p>
+                </div>
+            </div>
+          </div>
+
+          <div class="good-item" >
+            <div><div class="good-img">
+              <a href="/goodsDetails?productId=100022201" class="">
+              <img alt="《深泽直人》" src="http://image.smartisanos.cn/resource/5e4e40120d09fb6791f9430f914c6f68.jpg" lazy="loaded">
+              </a>
+              </div> 
+              <h6 class="good-title">《深泽直人》</h6> 
+              <h3 class="sub-title ellipsis">首次面向中国读者介绍其作品</h3> 
+              <div class="good-price pr">
+                <div class="ds pa">
+                <a href="/goodsDetails?productId=100022201" class="">
+                  <input data-v-02725a06="" type="button" readonly="readonly" class="default-btn" value="查看详情" style="margin: 0px 5px;">
+                </a> 
+                <input data-v-02725a06="" type="button" readonly="readonly" class="main-btn" value="加入购物车" style="margin: 0px 5px;">
+                </div>
+                  <p><span style="font-size: 16px;">￥</span>199</p>
+                </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
@@ -116,11 +155,129 @@ section{
 .card_cont_left{
   height: 100%;
   width: 600px;
-  border: 1px solid saddlebrown
+}
+.card_cont_left img{
+  width: 100%;
+  height: 100%;
 }
 .card_cont_right{
+  display: flex;
   height: 100%;
   width: 600px;
-  border: 1px solid saddlebrown
+}
+.card_cont_right ul{
+  height: 100%;
+  display: flex;
+}
+.card_cont_right ul li{
+  width:50%;
+  height:100%;
+}
+.goods{
+  width: 100%;
+  height: 100%;
+}
+.good-item{
+    background: #fff;
+    width: 50%;
+    transition: all .5s;
+}
+.good-item .good-img img{
+    margin: 50px auto 10px;
+    width: 206px;
+    height: 206px;
+    display: block;
+}
+.good-item .good-title {
+    line-height: 1.2;
+    font-size: 16px;
+    color: #424242;
+    margin: 0 auto;
+    padding: 0 14px;
+    text-align: center;
+    overflow: hidden;
+}
+.good-item h3{
+    text-align: center;
+    line-height: 1.2;
+    font-size: 12px;
+    color: #d0d0d0;
+    padding: 10px;
+}
+.ellipsis {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+}
+.good-item .good-price {
+    margin: 15px 0;
+    height: 30px;
+    text-align: center;
+    line-height: 30px;
+    color: #e4393c;
+    font-size: 20px;
+}
+.good-item .ds{
+    width: 100%;
+    display: none;
+}
+a {
+    font-style: normal;
+    text-decoration: none;
+    color: #5079d9;
+    cursor: pointer;
+    transition: all .15s ease-out;
+}
+.good-item .ds{
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+}
+.main-btn{
+    border: 1px solid #5c81e3;
+    border-radius: 4px;
+    font-size: 12px;
+    color: #fff;
+    background-color: #678ee7;
+    background-image: linear-gradient(180deg,#678ee7,#5078df);
+}
+.default-btn{
+    border: 1px solid #e1e1e1;
+    border-radius: 4px;
+    font-size: 12px;
+    color: #646464;
+    background-color: #f9f9f9;
+    background-image: linear-gradient(180deg,#fff,#f9f9f9);
+}
+.pr {
+    position: relative;
+}
+.pa {
+    position: absolute;
+}
+.good-item .good-price{
+    margin: 15px 0;
+    height: 30px;
+    text-align: center;
+    line-height: 30px;
+    color: #e4393c;
+    font-size: 20px;
+}
+.default-btn, .disabled-btn, .main-btn{
+    width: 100px;
+    height: 30px;
+    line-height: 28px;
+    vertical-align: middle;
+}
+input{
+    display: inline-block;
+    cursor: pointer;
+    text-align: center;
 }
 </style>
